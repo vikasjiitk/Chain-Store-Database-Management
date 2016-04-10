2f84a59987a632dbf1ce4966b0ebe84b327e0cfe
Krishna8021 <chkrish@cse.iitk.ac.in>
2016-04-10 23:39:06 +0530

    Added receptionist.php

---
+++ receptionist.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<?php session_start();?>
+<title>Receptionst</title>
+<head>
+  <link rel="stylesheet" type="text/css" href="style.css">
+<style>
+</style>
+</head>
+<body>
+   <p>
+   <h1> Receptionist </h1>
+   <h3></h3>
+   </p>   
+    <ul>
+      <li><a class="active" href="receptionist.php">Receptionst</a></li>
+      <li><a href="update.php">Update Stock</a></li>
+      <li><a href="selling.php">Selling Details</a></li>
+    </ul>
+</body>
